8a59b3688925266ce67077b3a88037975f9a8dae
Moussaab Djeradi <fm_djeradi@esi.dz>
2023-05-28 10:29:26 +0100

    Initial Commit

---
+++ Fitness App/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Fitness App
+//
+//  Created by Moussaab Djeradi on 28/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
